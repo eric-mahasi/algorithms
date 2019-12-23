510155ad3c96ec5a29aa927f8e3ef0b046cfb29c
Eric Mahasi <mahasieric@gmail.com>
2019-12-24 01:14:05 +0300

    Optimize code
    Changed the function signature for better code quality and to allow
    proper unit testing.
    
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- algorithms/ordered_insert.py
+++ algorithms/ordered_insert.py
@@ -2,21 +2,28 @@
 
 import bisect
 
-AGES = [1, 3, 7, 10, 11]
+AGES = []
 
 
-def ordered_insert():
+def ordered_insert(array):
     """Insert item into a list, and keep it sorted assuming the list is
     already sorted. If the item is already in the list, insert it to the
     right of the rightmost item.
+
+    Parameters
+    ----------
+    array : iterable
+        A list of unsorted number
     """
     count = int(input("How many ages do you want to enter? "))
     for i in range(count):
         x = int(input("Enter an age: "))
-        bisect.insort(AGES, x)
+        bisect.insort(array, x)
+
+    return array
 
 
 if __name__ == '__main__':
     print("The original list is : " + str(AGES))
-    ordered_insert()
+    print(ordered_insert(AGES))
     print("The list after insertion is : " + str(AGES))
