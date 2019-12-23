eb5fb4ea79684dc7e11f5c52dd98e36c0ab1c67a
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:22:54 +0300

    Rename list
    I renamed the list in order to comply to Pylint conventions.
    
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,13 @@
 """This script demonstrates the binary search algorithm in Python 3."""
 
-ages = [1, 3, 7, 10, 11, 13, 15, 16, 19, 20]
+AGES = [1, 3, 7, 10, 11, 13, 15, 16, 19, 20]
 
 
 def display():
     """Prints all the items in the list."""
     print("The ages are: ")
-    for i in range(len(ages)):
-        print(ages[i])
+    for i in range(len(AGES)):
+        print(AGES[i])
 
 
 def binary_search(x):
@@ -15,17 +15,17 @@ def binary_search(x):
     complexity of O(log n).
     """
     lower_bound = 0
-    upper_bound = len(ages) - 1
+    upper_bound = len(AGES) - 1
     while True:
         mid = int((lower_bound + upper_bound) / 2)
-        if x == ages[mid]:
+        if x == AGES[mid]:
             print("A student aged", x, "was found.")
             return True
         elif lower_bound > upper_bound:
             print("A student aged", x, "was not found.")
             return False
         else:
-            if x < ages[mid]:
+            if x < AGES[mid]:
                 upper_bound = mid - 1
             else:
                 lower_bound = mid + 1
