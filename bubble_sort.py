13bb10e4d7069214f7a88fb7311a4624d4ef8252
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:17:58 +0300

    Rename list
    I renamed the list in order to comply to Pylint conventions.
    
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,13 @@
 """This script demonstrates the bubble sort algorithm in Python 3."""
 
-ages = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
+AGES = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
 
 
 def display():
     """Prints all the items in the list."""
     print("The ages are: ")
-    for i in range(len(ages)):
-        print(ages[i])
+    for i in range(len(AGES)):
+        print(AGES[i])
     print("\n")
 
 
@@ -15,10 +15,10 @@ def bubble_sort():
     """Repeatedly step through the list, compare adjacent items and swap
     them if they are in the wrong order. Time complexity of O(n^2).
     """
-    for i in range(len(ages) - 1):
-        for j in range(len(ages) - 1 - i):
-            if ages[j] > ages[j + 1]:
-                ages[j], ages[j + 1] = ages[j + 1], ages[j]
+    for i in range(len(AGES) - 1):
+        for j in range(len(AGES) - 1 - i):
+            if AGES[j] > AGES[j + 1]:
+                AGES[j], AGES[j + 1] = AGES[j + 1], AGES[j]
 
 
 if __name__ == '__main__':
