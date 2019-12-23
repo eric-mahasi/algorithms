8c0472da53783512b110804ce5c37b1713bd5d62
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:32:17 +0300

    Rename list
    I renamed the list in order to comply to Pylint conventions.
    
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- linear_search.py
+++ linear_search.py
@@ -1,21 +1,21 @@
 """This script demonstrates the linear search algorithm in Python 3."""
 
-ages = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
+AGES = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
 
 
 def display():
     """Prints all the items in the list."""
     print("The ages are: ")
-    for i in range(len(ages)):
-        print(ages[i])
+    for i in range(len(AGES)):
+        print(AGES[i])
 
 
 def linear_search(x):
     """Sequentially check each element of the list until a match is found or
     until the whole list has been searched. Time complexity of O(n).
     """
-    for i in range(len(ages)):
-        if x == ages[i]:
+    for i in range(len(AGES)):
+        if x == AGES[i]:
             print("A student aged ", x, " was found.")
             return True
         else:
