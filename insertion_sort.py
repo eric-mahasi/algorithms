065585da6ededa853e97965919790502100f60fc
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:10:21 +0300

    Add script docstrings
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,21 +1,27 @@
-ages = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
+"""This script demonstrates the insertion sort algorithm in Python 3."""
+
+AGES = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
 
 
 def display():
+    """Prints all the items in the list."""
     print("The ages are: ")
-    for i in range(len(ages)):
-        print(ages[i])
+    for i in range(len(AGES)):
+        print(i)
     print("\n")
 
 
 def insertion_sort():
-    for i in range(len(ages)):
-        temp = ages[i]
+    """Build the final sorted list one item at a time. Time complexity of
+    O(n^2).
+    """
+    for i in range(len(AGES)):
+        temp = AGES[i]
         j = i
-        while j > 0 and ages[j - 1] >= temp:
-            ages[j] = ages[j - 1]
+        while j > 0 and AGES[j - 1] >= temp:
+            AGES[j] = AGES[j - 1]
             j = j - 1
-        ages[j] = temp
+        AGES[j] = temp
 
 
 if __name__ == '__main__':
