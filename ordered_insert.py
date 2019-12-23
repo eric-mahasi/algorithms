83f4d78b6f7da379c9bfe9bafe87174afc756825
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 20:02:05 +0300

    Add script docstrings
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- ordered_insert.py
+++ ordered_insert.py
@@ -1,9 +1,15 @@
+"""This script demonstrates the ordered insert algorithm in Python 3."""
+
 import bisect
 
 ages = [1, 3, 7, 10, 11]
 
 
 def ordered_insert():
+    """Insert item into a list, and keep it sorted assuming the list is
+    already sorted. If the item is already in the list, insert it to the
+    right of the rightmost item.
+    """
     count = int(input("How many ages do you want to enter? "))
     for i in range(count):
         x = int(input("Enter an age: "))
