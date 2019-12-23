d8eadca77062211d2f330e3b3ec71c84dcfa3f74
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:14:29 +0300

    Add script docstrings
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,10 @@
+"""This script demonstrates the bubble sort algorithm in Python 3."""
+
 ages = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
 
 
 def display():
+    """Prints all the items in the list."""
     print("The ages are: ")
     for i in range(len(ages)):
         print(ages[i])
@@ -9,6 +12,9 @@ def display():
 
 
 def bubble_sort():
+    """Repeatedly step through the list, compare adjacent items and swap
+    them if they are in the wrong order. Time complexity of O(n^2).
+    """
     for i in range(len(ages) - 1):
         for j in range(len(ages) - 1 - i):
             if ages[j] > ages[j + 1]:
