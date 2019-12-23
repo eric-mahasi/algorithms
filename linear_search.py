3ccdcc26f5dc19b5dc8dd8d97130b691ef900801
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 19:30:53 +0300

    Add script docstrings
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- linear_search.py
+++ linear_search.py
@@ -1,13 +1,19 @@
+"""This script demonstrates the linear search algorithm in Python 3."""
+
 ages = [19, 20, 13, 1, 15, 7, 11, 3, 16, 10]
 
 
 def display():
+    """Prints all the items in the list."""
     print("The ages are: ")
     for i in range(len(ages)):
         print(ages[i])
 
 
 def linear_search(x):
+    """Sequentially check each element of the list until a match is found or
+    until the whole list has been searched. Time complexity of O(n).
+    """
     for i in range(len(ages)):
         if x == ages[i]:
             print("A student aged ", x, " was found.")
