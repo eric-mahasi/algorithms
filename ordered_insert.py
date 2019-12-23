8375d12f9228913403cec32e6b85f26b2d488bdf
Eric Mahasi <mahasieric@gmail.com>
2019-12-23 20:02:24 +0300

    Rename list
    I renamed the list in order to comply to Pylint conventions.
    
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- ordered_insert.py
+++ ordered_insert.py
@@ -2,7 +2,7 @@
 
 import bisect
 
-ages = [1, 3, 7, 10, 11]
+AGES = [1, 3, 7, 10, 11]
 
 
 def ordered_insert():
@@ -13,10 +13,10 @@ def ordered_insert():
     count = int(input("How many ages do you want to enter? "))
     for i in range(count):
         x = int(input("Enter an age: "))
-        bisect.insort(ages, x)
+        bisect.insort(AGES, x)
 
 
 if __name__ == '__main__':
-    print("The original list is : " + str(ages))
+    print("The original list is : " + str(AGES))
     ordered_insert()
-    print("The list after insertion is : " + str(ages))
+    print("The list after insertion is : " + str(AGES))
